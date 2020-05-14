04f9ff3fd040f0aaf46f5e576ed5d084b29d821c
Daniel_Cho <hmc0105@kaist.ac.kr>
2020-05-14 15:36:43 +0900

    final

--- predict.py
+++ predict.py
@@ -1,7 +1,22 @@
 import tensorflow as tf
+import cv2
+import numpy as np
+import pandas as pd
 
 def predict(path):
-    model=load_model('model.h5')
-
-    model.predict(path)
-    
+    image_array=[]
+    img=cv2.imread(path)
+    img=cv2.resize(img, dsize=(299,299))
+    img=img/255.0
+    image_array.append(img)
+    image_array=np.array(image_array)
+    model=tf.keras.models.load_model('model.h5')
+    result=model.predict(image_array)[0].argsort()[::-1][:5]
+    label_text=pd.read_csv('dataset/labels.csv')
+    unique_Y=label_text['breed'].unique().tolist()
+    unique_sorted_Y=sorted(unique_Y)
+    print('top1 : ',unique_sorted_Y[result[0]])
+    print('top2 : ',unique_sorted_Y[result[1]])
+    print('top3 : ',unique_sorted_Y[result[2]])
+    print('top4 : ',unique_sorted_Y[result[3]])
+    print('top5 : ',unique_sorted_Y[result[4]])
--- train.py
+++ train.py
@@ -1,25 +1,59 @@
 import tensorflow as tf
 from tensorflow.keras.applications import InceptionV3
 from image_aug import *
+import os
+import pandas as pd
+import shutil
+import numpy as np
 
 def train():
-    train_X,train_Y,valid_X,valid_Y=image_aug()
-    print(train_X)
+    current_path=os.getcwd()
+    data_path=current_path+"/dataset"
+
+    label_text=pd.read_csv((data_path+'/labels.csv'))
+
+    train_sub_path=current_path+'/train_sub/'
+    if not os.path.isdir(train_sub_path):
+        os.mkdir(train_sub_path)
+
+    for i in range(len(label_text)):
+        if os.path.exists((train_sub_path+label_text.loc[i]['breed']))==False:
+            os.mkdir((train_sub_path+label_text.loc[i]['breed']))
+        shutil.copy((data_path+'/train/train/'+label_text.loc[i]['id']+'.jpg'),(train_sub_path+label_text.loc[i]['breed']))
+
+    from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+
+
+ #image generating
+    image_size=299
+    batch_size=32
+
+    train_datagen=ImageDataGenerator(rescale=1./255.,horizontal_flip=True,shear_range=0.2,zoom_range=0.2,
+    width_shift_range=0.2,height_shift_range=0.2,validation_split=0.25)
+    valid_datagen=ImageDataGenerator(rescale=1./255.,validation_split=0.25)
+ 
+    train_generator=train_datagen.flow_from_directory(directory=train_sub_path,subset="training",batch_size=batch_size,
+    seed=42,shuffle=True,class_mode='categorical',target_size=(image_size,image_size))
+
+    valid_generator=valid_datagen.flow_from_directory(directory=train_sub_path,subset='validation',batch_size=1,seed=42,
+    shuffle=True,class_mode='categorical',target_size=(image_size,image_size))
+
+    
     inceptionv3=InceptionV3()
 
     print(1)
-    for layer in inceptionv3.layers[:-10]:
+    for layer in inceptionv3.layers[:-5]:
         layer.trainable=False
-    for layer in inceptionv3.layers[-10:]:
+    for layer in inceptionv3.layers[-5:]:
         layer.trainable=True
     print(2)
     x=inceptionv3.layers[-2].output
     predictions=tf.keras.layers.Dense(120,activation='softmax')(x)
     model=tf.keras.Model(inputs=inceptionv3.input,outputs=predictions)
 
-    model.compile(optimizer='sgd',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+    model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])
     print(5)
-    hisotry=model.fit(train_X,train_Y,epochs=10,validation_split=0.25,batch_size=32)
+    history=model.fit_generator(generator=train_generator,steps_per_epoch=10,epochs=50,validation_data=valid_generator,validation_steps=3)
     print(3)
 
     model.save('model.h5')
