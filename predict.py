23a3a29eeeb949e19948e0f703c91baebece5b9c
Daniel_Cho <hmc0105@kaist.ac.kr>
2020-05-21 13:01:44 +0900

    Write

--- predict.py
+++ predict.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 def predict(path):
     image_array=[]
-    img=cv2.imread(path)
+    img=cv2.imread(path,1)
+    org=img
     img=cv2.resize(img, dsize=(299,299))
     img=img/255.0
     image_array.append(img)
@@ -15,8 +16,14 @@ def predict(path):
     label_text=pd.read_csv('dataset/labels.csv')
     unique_Y=label_text['breed'].unique().tolist()
     unique_sorted_Y=sorted(unique_Y)
-    print('top1 : ',unique_sorted_Y[result[0]])
-    print('top2 : ',unique_sorted_Y[result[1]])
-    print('top3 : ',unique_sorted_Y[result[2]])
-    print('top4 : ',unique_sorted_Y[result[3]])
-    print('top5 : ',unique_sorted_Y[result[4]])
+    top1='top1 : '+unique_sorted_Y[result[0]]
+    top2='top2 : '+unique_sorted_Y[result[1]]
+    top3='top3 : '+unique_sorted_Y[result[2]]
+    top4='top4 : '+unique_sorted_Y[result[3]]
+    top5='top5 : '+unique_sorted_Y[result[4]]
+    cv2.putText(org, top1,(0,20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255))
+    cv2.putText(org, top2,(0,40),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255))
+    cv2.putText(org, top3,(0,60),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255))
+    cv2.putText(org, top4,(0,80),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255))
+    cv2.putText(org, top5,(0,100),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255))
+    cv2.imwrite("output.jpg",org)
